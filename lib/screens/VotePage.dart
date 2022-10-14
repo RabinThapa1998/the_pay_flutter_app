42f2c25734914d331fc7fea02e4377737f28fb86
RabinThapa1998 <imrabin1998@gmail.com>
2022-10-14 23:21:12 +0545

    💄 :Style: Update Vote Page

--- lib/screens/VotePage.dart
+++ lib/screens/VotePage.dart
@@ -16,56 +16,71 @@ class _VotePageState extends State<VotePage> {
         appBar: AppBar(
           title: const Text('Vote'),
         ),
-        body: Column(
-          children: [
-            Card(
-              child: Row(
-                children: [
-                  Text(Get.arguments.toString()),
-                  Icon(Icons.arrow_forward_ios),
-                ],
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage:
+                            AssetImage('assets/images/contestant.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(Get.arguments.toString()),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Text(
-              "Packages",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        leading: Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                              color: Colors.orange,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text("2",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                              Text("Votes",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                            ],
+              Text(
+                "Packages",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          leading: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text("2",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                                Text("Votes",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ],
+                            ),
                           ),
-                        ),
-                        title: const Text('Rs 12'),
-                        subtitle: const Text('Submit 2 votes for Rs 12'),
-                        trailing: const Icon(Icons.arrow_forward_ios),
-                      );
-                    }))
-          ],
+                          title: const Text('Rs 12'),
+                          subtitle: const Text('Submit 2 votes for Rs 12'),
+                          trailing: const Icon(Icons.arrow_forward_ios),
+                        );
+                      }))
+            ],
+          ),
         ),
       ),
     );
