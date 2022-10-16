b501d237807c0001898b9430f972d7bc6109af85
RabinThapa1998 <imrabin1998@gmail.com>
2022-10-16 15:55:20 +0545

    💄:Style: Fix VotePage Design

--- lib/screens/VotePage.dart
+++ lib/screens/VotePage.dart
@@ -26,77 +26,86 @@ class _VotePageState extends State<VotePage> {
         appBar: AppBar(
           title: const Text('Vote'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Card(
-              margin: const EdgeInsets.all(10),
-              elevation: 0,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      child: CircleAvatar(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                elevation: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
                         radius: 50,
-                        backgroundImage:
-                            AssetImage('assets/images/contestant.jpg'),
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage:
+                              AssetImage('assets/images/contestant.jpg'),
+                        ),
                       ),
-                    ),
-                    Text(Get.arguments != null
-                        ? Get.arguments["constestant"].toString()
-                        : 'loading...'),
-                  ],
+                      Text(Get.arguments != null
+                          ? Get.arguments["constestant"].toString()
+                          : 'loading...'),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Text(
-              "Packages",
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey),
-            ),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: paymentSchema.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        leading: Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                              color: Colors.orange,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(paymentSchema[index]["votes"].toString(),
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                              Text("Votes",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                            ],
+              Text(
+                "Packages",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: paymentSchema.length,
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                          child: ListTile(
+                            tileColor: Colors.white,
+                            leading: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                  color: Colors.orange,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(paymentSchema[index]["votes"].toString(),
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                  Text("Votes",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                ],
+                              ),
+                            ),
+                            title:
+                                Text(paymentSchema[index]["votes"].toString()),
+                            subtitle: Text(
+                                'Submit ${paymentSchema[index]["votes"].toString()} votes for Rs ${paymentSchema[index]["cost"].toString()}'),
+                            trailing: const Icon(Icons.arrow_forward_ios),
                           ),
-                        ),
-                        title: Text(paymentSchema[index]["votes"].toString()),
-                        subtitle: Text(
-                            'Submit ${paymentSchema[index]["votes"].toString()} votes for Rs ${paymentSchema[index]["cost"].toString()}'),
-                        trailing: const Icon(Icons.arrow_forward_ios),
-                      );
-                    }))
-          ],
+                        );
+                      }))
+            ],
+          ),
         ),
       ),
     );
